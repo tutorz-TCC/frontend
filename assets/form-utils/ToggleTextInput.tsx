f666176d131230a5cb81b3f1b98fad7ffdd002db
Jonatas de Almeida <jonatasdealmeida2017@gmail.com>
2024-10-09 19:17:57 -0300

    Criação de pasta dedicada a recursos e componentes utilitários do projeto

---
+++ assets/form-utils/ToggleTextInput.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState } from "react";
+import {
+	Image,
+	TextInput,
+	View,
+	TextInputProps,
+	ImageSourcePropType,
+	TouchableOpacity,
+} from "react-native";
+import formStyle from "../../styles/formStyles";
+
+interface ToggleTextInputProps extends TextInputProps {
+	iconSource: {
+		active: ImageSourcePropType;
+		inactive: ImageSourcePropType;
+	};
+	placeholder?: string;
+}
+
+const ToggleTextInput: React.FC<ToggleTextInputProps> = ({
+	iconSource,
+	placeholder,
+}) => {
+	const [seguranca, setSeguranca] = useState(true);
+	return (
+		<View
+			style={{
+				width: "100%",
+				display: "flex",
+				flexDirection: "row",
+			}}
+		>
+			<TextInput
+				placeholder={placeholder}
+				secureTextEntry={seguranca}
+				style={[
+					formStyle.input,
+					{
+						flex: 9,
+
+						borderBottomRightRadius: 0,
+						borderBottomLeftRadius: 0,
+					},
+				]}
+			/>
+			<TouchableOpacity
+				onPress={() => setSeguranca(!seguranca)}
+				style={{
+					flex: 1,
+
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+
+					backgroundColor: "#FFFFFF",
+
+					borderBottomRightRadius: 10,
+					borderTopRightRadius: 10,
+				}}
+			>
+				<Image
+					source={seguranca ? iconSource.active : iconSource.inactive}
+					style={{ height: 20, width: 20 }}
+				/>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default ToggleTextInput;
