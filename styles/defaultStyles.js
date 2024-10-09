c34b339e45681286216234e7f50aac833c8d1aa5
Jonatas de Almeida <jonatasdealmeida2017@gmail.com>
2024-10-09 19:16:00 -0300

    Adição de planihas de estilo para personalização da interface de usuário

---
+++ styles/defaultStyles.js
@@ -0,0 +1,24 @@
+//Estilos padrão do projeto
+import { StyleSheet } from "react-native";
+
+const defaultStyles = StyleSheet.create({
+	//Paleta de cores principais do design
+	azul_noturno:{
+		backgroundColor:"#0C0635",
+		color:"#0C0635"
+	},
+	azul_celeste:{
+		backgroundColor:"#41AFDC",
+		color:"#41AFDC"
+	},
+	azul_marinho:{
+		backgroundColor:"#1F2E79",
+		color:"#1F2E79"
+	},
+	azul_real:{
+		backgroundColor:"#2F63A2",
+		color:"#2F63A2"
+	}
+})
+
+export default defaultStyles;
---
+++ styles/formStyles.js
@@ -0,0 +1,22 @@
+//Estilos orientados aos formulários do projeto
+import { StyleSheet } from "react-native";
+
+const formStyle = StyleSheet.create({
+	input: {
+		height: 40,
+		borderRadius: 10,
+		backgroundColor: "#FFFFFF",
+		borderColor: "#FFFFFF",
+	},
+	submit_button: {
+		height: 50,
+		borderRadius: 15,
+	},
+	default_font: {
+		color: "#000000",
+
+		fontSize: 16,
+	},
+});
+
+export default formStyle;
