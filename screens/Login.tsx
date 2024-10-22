3d2a280660d2542f3d9fcc8c58be85e651b62d2f
Jonatas de Almeida <jonatasdealmeida2017@gmail.com>
2024-10-21 21:29:38 -0300

    Finalização da Tela de Cadastro

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,27 +1,26 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import ToggleTextInput from "../assets/form-utils/ToggleTextInput";
-import {
-  Keyboard,
-  Image,
-  KeyboardAvoidingView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-  useWindowDimensions,
-} from "react-native";
-
 import defaultStyles from "../styles/defaultStyles";
 import formStyle from "../styles/formStyles";
 
+import React, { useEffect, useState } from "react";
+import ToggleTextInput from "../assets/form-utils/ToggleTextInput";
+
+import {
+  Keyboard,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
 const Login = () => {
   const [teclado, setTeclado] = useState<boolean>(false);
+
   const olho = {
     active: require("../assets/source-images/eye.png"),
     inactive: require("../assets/source-images/eye-off.png"),
   };
+
   useEffect(() => {
     const tecladoVisivel = Keyboard.addListener("keyboardDidShow", () => {
       setTeclado(true);
@@ -30,6 +29,7 @@ const Login = () => {
       setTeclado(false);
     });
   }, []);
+
   return (
     <View
       onTouchStart={Keyboard.dismiss}
@@ -43,55 +43,6 @@ const Login = () => {
     >
       <View
         style={[
-          defaultStyles.forma_oval,
-          defaultStyles.azul_noturno,
-          {
-            borderTopLeftRadius: 0,
-            borderTopRightRadius: 0,
-            width: "50%",
-            height: useWindowDimensions().width / 1.5,
-            top: "-10%",
-          },
-        ]}
-      ></View>
-      <View
-        style={[
-          defaultStyles.forma_oval,
-          defaultStyles.azul_celeste,
-          {
-            width: "50%",
-            height: useWindowDimensions().width / 2,
-            right: 0,
-            top: "9%",
-          },
-        ]}
-      ></View>
-      <View
-        style={[
-          defaultStyles.forma_oval,
-          defaultStyles.azul_marinho,
-          {
-            width: "50%",
-            height: useWindowDimensions().width / 2,
-            left: 0,
-            top: "5%",
-          },
-        ]}
-      ></View>
-      <View
-        style={[
-          defaultStyles.forma_oval,
-          defaultStyles.azul_real,
-          {
-            width: "50%",
-            height: useWindowDimensions().width / 2,
-            right: 0,
-            top: "2%",
-          },
-        ]}
-      ></View>
-      <View
-        style={[
           defaultStyles.azul_noturno,
           {
             width: "90%",
@@ -99,8 +50,6 @@ const Login = () => {
             gap: 15,
             padding: "5%",
             borderRadius: 20,
-            position: "absolute",
-            bottom: "1%",
           },
         ]}
       >
@@ -109,6 +58,7 @@ const Login = () => {
             <Image source={require("../assets/source-images/invertL.png")} />
           )}
         </View>
+
         <Text style={defaultStyles.fonte_invertida}>Login</Text>
         <TextInput
           autoComplete="email"
@@ -116,29 +66,34 @@ const Login = () => {
           placeholderTextColor="#777"
           style={formStyle.input}
         />
+
         <Text style={defaultStyles.fonte_invertida}>Senha</Text>
         <ToggleTextInput
           autoComplete="password"
           iconSource={olho}
           placeholder="Por favor, insira sua senha"
         ></ToggleTextInput>
+
         <TouchableOpacity style={formStyle.submit_button}>
-          <Text style={defaultStyles.fonte_titulo_estilizado}>Entrar</Text>
+          <Text style={defaultStyles.fonte_titulo_estilizado}>ENTRAR</Text>
         </TouchableOpacity>
+
         {!teclado && (
-          <Text
-            style={[
-              defaultStyles.fonte_invertida,
-              defaultStyles.destaque,
-              {
-                textDecorationLine: "underline",
-                fontSize: 14,
-                textAlign: "center",
-              },
-            ]}
-          >
-            Clique aqui para realizar o cadastro
-          </Text>
+          <TouchableOpacity>
+            <Text
+              style={[
+                defaultStyles.fonte_invertida,
+                defaultStyles.destaque,
+                {
+                  textDecorationLine: "underline",
+                  fontSize: 14,
+                  textAlign: "center",
+                },
+              ]}
+            >
+              Clique aqui para realizar o cadastro
+            </Text>
+          </TouchableOpacity>
         )}
       </View>
     </View>
