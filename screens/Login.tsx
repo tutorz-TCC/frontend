7575e1c207534645a76034aeec2d65a0c14b9b7c
Jonatas de Almeida <jonatasdealmeida2017@gmail.com>
2024-11-22 00:47:19 -0300

    correção das rotas para com os ajustes no servidor

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -5,145 +5,149 @@ import React, { useEffect, useState } from "react";
 import ToggleTextInput from "../assets/components/ToggleTextInput";
 
 import {
-  Keyboard,
-  Image,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
+	Keyboard,
+	Image,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
 } from "react-native";
 import Background from "../assets/components/Background";
 import { useNavigation } from "@react-navigation/native";
 import EncryptedStorage from "react-native-encrypted-storage";
 
 const Login = () => {
-  const [teclado, setTeclado] = useState<boolean>(false);
-  const [usuario, setUsuario] = useState<string>("");
-  const [senha, setSenha] = useState<string>("");
+	const [teclado, setTeclado] = useState<boolean>(false);
+	const [usuario, setUsuario] = useState<string>("");
+	const [senha, setSenha] = useState<string>("");
 
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  type autenticacao = {
-    usuario: string;
-    senha: string;
-  };
+	type autenticacao = {
+		usuario: string;
+		senha: string;
+	};
 
-  const gerenciadorLogin = async ({ usuario, senha }: autenticacao) => {
-    try {
-      const token = await fetch("http://127.0.0.1:8080/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          loginuser: usuario,
-          password: senha,
-        }),
-      })
-        .then((dados) => {
-          if (dados.ok) {
-            return dados.json();
-          } else {
-            throw new Error("Erro na requisição");
-          }
-        })
-        .then((dados) => JSON.stringify(dados.token,null,2) )
-        .catch((erro) => console.log(erro));
+	const gerenciadorLogin = async ({ usuario, senha }: autenticacao) => {
+		try {
+			const token = await fetch("http://127.0.0.1:8080/api/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: usuario,
+					password: senha,
+				}),
+			})
+				.then((dados) => {
+					if (dados.ok) {
+						return dados.json();
+					} else {
+						 
+						console.log(usuario);
+						console.log(senha);
+						console.log(dados);
+						throw new Error("Erro na requisição");
+					}
+				})
+				.then((dados) => JSON.stringify(dados.token, null, 2))
+				.catch((erro) => console.log(erro));
 
-      if (typeof token === "string") {
-        await EncryptedStorage.setItem("token", token);
-      } else {
-        throw new Error("O usuário não existe");
-      }
+			if (typeof token === "string") {
+				await EncryptedStorage.setItem("token", token);
+			} else {
+				throw new Error("O usuário não autenticado");
+			}
 
-      navigation.navigate("Main")
-    } catch (error) {
-      console.log(error);
-    }
-  };
+			navigation.navigate("Main");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const olho = {
-    active: require("../assets/source-images/eye.png"),
-    inactive: require("../assets/source-images/eye-off.png"),
-  };
+	const olho = {
+		active: require("../assets/source-images/eye.png"),
+		inactive: require("../assets/source-images/eye-off.png"),
+	};
 
-  useEffect(() => {
-    const tecladoVisivel = Keyboard.addListener("keyboardDidShow", () => {
-      setTeclado(true);
-    });
-    const tecladoInvisivel = Keyboard.addListener("keyboardDidHide", () => {
-      setTeclado(false);
-    });
-  }, []);
+	useEffect(() => {
+		const tecladoVisivel = Keyboard.addListener("keyboardDidShow", () => {
+			setTeclado(true);
+		});
+		const tecladoInvisivel = Keyboard.addListener("keyboardDidHide", () => {
+			setTeclado(false);
+		});
+	}, []);
 
-  return (
-    <Background>
-      <View
-        style={[
-          defaultStyles.azul_noturno,
-          {
-            width: "90%",
-            display: "flex",
-            gap: 15,
-            padding: "5%",
-            borderRadius: 20,
-          },
-        ]}
-      >
-        <View style={{ display: "flex", alignItems: "center" }}>
-          {!teclado && (
-            <Image source={require("../assets/source-images/invertL.png")} />
-          )}
-        </View>
+	return (
+		<Background>
+			<View
+				style={[
+					defaultStyles.azul_noturno,
+					{
+						width: "90%",
+						display: "flex",
+						gap: 15,
+						padding: "5%",
+						borderRadius: 20,
+					},
+				]}
+			>
+				<View style={{ display: "flex", alignItems: "center" }}>
+					{!teclado && (
+						<Image source={require("../assets/source-images/invertL.png")} />
+					)}
+				</View>
 
-        <Text style={defaultStyles.fonte_invertida}>Usuario</Text>
-        <TextInput
-          autoComplete="email"
-          placeholder="Por favor, insira seu e-mail"
-          placeholderTextColor="#777"
-          style={formStyle.input}
-          onChangeText={setUsuario}
-          value={usuario}
-        />
+				<Text style={defaultStyles.fonte_invertida}>Usuario</Text>
+				<TextInput
+					autoComplete="email"
+					placeholder="Por favor, insira seu e-mail"
+					placeholderTextColor="#777"
+					style={formStyle.input}
+					onChangeText={setUsuario}
+					value={usuario}
+				/>
 
-        <Text style={defaultStyles.fonte_invertida}>Senha</Text>
-        <ToggleTextInput
-          autoComplete="password"
-          iconSource={olho}
-          placeholder="Por favor, insira sua senha"
-          onChangeText={setSenha}
-          value={senha}
-        ></ToggleTextInput>
+				<Text style={defaultStyles.fonte_invertida}>Senha</Text>
+				<ToggleTextInput
+					autoComplete="password"
+					iconSource={olho}
+					placeholder="Por favor, insira sua senha"
+					onChangeText={setSenha}
+					value={senha}
+				></ToggleTextInput>
 
-        <TouchableOpacity
-          style={formStyle.submit_button}
-          onPress={() => {
-            gerenciadorLogin({ usuario, senha });
-          }}
-        >
-          <Text style={defaultStyles.fonte_titulo_estilizado}>ENTRAR</Text>
-        </TouchableOpacity>
+				<TouchableOpacity
+					style={formStyle.submit_button}
+					onPress={() => {
+						gerenciadorLogin({ usuario, senha });
+					}}
+				>
+					<Text style={defaultStyles.fonte_titulo_estilizado}>ENTRAR</Text>
+				</TouchableOpacity>
 
-        {!teclado && (
-          <TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
-            <Text
-              style={[
-                defaultStyles.fonte_invertida,
-                defaultStyles.destaque,
-                {
-                  textDecorationLine: "underline",
-                  fontSize: 14,
-                  textAlign: "center",
-                },
-              ]}
-            >
-              Clique aqui para realizar o cadastro
-            </Text>
-          </TouchableOpacity>
-        )}
-      </View>
-    </Background>
-  );
+				{!teclado && (
+					<TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
+						<Text
+							style={[
+								defaultStyles.fonte_invertida,
+								defaultStyles.destaque,
+								{
+									textDecorationLine: "underline",
+									fontSize: 14,
+									textAlign: "center",
+								},
+							]}
+						>
+							Clique aqui para realizar o cadastro
+						</Text>
+					</TouchableOpacity>
+				)}
+			</View>
+		</Background>
+	);
 };
 
 export default Login;
